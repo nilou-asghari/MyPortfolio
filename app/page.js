adbfe4afd03748c8e78b50eb599d6b4cd008777a
Niloufar Asghari <nilou.asghari@gmail.com>
2025-03-13 11:51:47 +0100

    initial commit

---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-blue-500 text-white text-3xl font-bold">
+      Tailwind CSS is working! 🚀
+    </div>
+  );
+}
